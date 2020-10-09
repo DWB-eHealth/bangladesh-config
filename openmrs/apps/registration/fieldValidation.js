4484f971a2f5fc8127dcd4d875924821d7ba048a
Michaël Bontyes <michaelbontyes@gmail.com>
2020-10-09 18:05:00 -0400

    registration page 1 - in progress

--- openmrs/apps/registration/fieldValidation.js
+++ openmrs/apps/registration/fieldValidation.js
@@ -4,17 +4,5 @@ Bahmni.Registration.customValidator = {
             return value >= 0;
         },
         errorMessage: "REGISTRATION_AGE_ERROR_KEY"
-    },
-    "Telephone Number": {
-        method: function (name, value, personAttributeDetails) {
-            return value && value.length> 6;
-        },
-        errorMessage: "REGISTRATION_TELEPHONE_NUMBER_ERROR_KEY"
-    },
-    "caste": {
-        method: function (name, value, personAttributeDetails) {
-            return value.match(/^\w+$/);
-        },
-        errorMessage: "REGISTRATION_CASTE_TEXT_ERROR_KEY"
     }
 };
