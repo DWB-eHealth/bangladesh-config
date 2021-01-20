9b62365e76e19ecca7554296b1cc75b4b0e6eb91
Michaël Bontyes <michaelbontyes@gmail.com>
2021-01-19 19:26:22 -0500

    Nested views removed

--- bahmni-mart/metabase-views/last_comorbidities_per_patient_id.sql
+++ bahmni-mart/metabase-views/last_comorbidities_per_patient_id.sql
@@ -1,35 +1,29 @@
 create view last_comorbidities_per_patient_id as
 select
-	pdv.patient_id ,
-	pdv.gender ,
-	pdd.age ,
+	pi.patient_id ,
 	pd2.visit_id ,
 	pd2.location_name ,
 	pd2.diagnosis_date ,
 	cmc.co_morbid_conditions
 from
-	patient_details_view pdv
-left outer join person_details_default pdd
-on pdd.person_id = pdv.person_id
+	patient_identifier pi
 left outer join patient_diagnosis pd2
-on pd2.patient_id = pdv.patient_id
+on pd2.patient_id = pi.patient_id
 and pd2.visit_id = (
 	select MAX(visit_id)
 	from patient_diagnosis pd3
-	where pd3.patient_id = pdv.patient_id and pd3.visit_id is not null
+	where pd3.patient_id = pi.patient_id and pd3.visit_id is not null
 )
 and pd2.diagnosis_date = (
 	select MAX(diagnosis_date)
 	from patient_diagnosis pd4
-	where pd4.patient_id = pdv.patient_id and pd4.diagnosis_date is not null
+	where pd4.patient_id = pi.patient_id and pd4.diagnosis_date is not null
 )
 left outer join co_morbid_conditions cmc
-on cmc.patient_id = pdv.patient_id and cmc.visit_id = pd2.visit_id
+on cmc.patient_id = pi.patient_id and cmc.visit_id = pd2.visit_id
 where pd2.visit_id is not null and pd2.diagnosis_date is not null
 group by
-	pdv.patient_id ,
-	pdv.gender ,
-	pdd.age ,
+	pi.patient_id ,
 	pd2.visit_id ,
 	pd2.location_name ,
 	pd2.diagnosis_date ,
---
+++ bahmni-mart/metabase-views/last_date_of_exit_per_patient_id.sql
@@ -0,0 +1,26 @@
+create view last_date_of_exit_per_patient_id as
+select
+	pi.patient_id ,
+	eae.visit_id ,
+	eae.date_of_entry_into_cohort ,
+	eae.date_of_exit_from_cohort
+from
+	patient_identifier pi
+left outer join entrance_and_exit eae
+on eae.patient_id = pi.patient_id
+and eae.visit_id = (
+	select MAX(visit_id)
+	from entrance_and_exit eae2
+	where eae2.patient_id = pi.patient_id and eae2.visit_id is not null
+)
+and eae.date_of_exit_from_cohort = (
+	select MAX(date_of_exit_from_cohort)
+	from entrance_and_exit eae3
+	where eae3.patient_id = pi.patient_id and eae3.date_of_exit_from_cohort is not null
+)
+where eae.visit_id is not null and eae.date_of_exit_from_cohort is not null
+group by
+	pi.patient_id ,
+	eae.visit_id ,
+	eae.date_of_entry_into_cohort ,
+	eae.date_of_exit_from_cohort
