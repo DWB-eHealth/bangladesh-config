daa7e5a98ed6887f54d9bc8e607b24f20c70cb72
Michaël Bontyes <michaelbontyes@gmail.com>
2021-02-02 11:30:20 -0500

    Create hba1c_ranges_view.sql

---
+++ bahmni-mart/viewSql/hba1c_ranges_view.sql
@@ -0,0 +1,13 @@
+SELECT
+	patient_id ,
+	visit_id ,
+	case
+  	when hba1c <=6.5 then
+  	'<=6.5'
+		when hba1c between 6.6 and 8.0 then
+  	'6.6-8.0'
+		when hba1c >=8.1 then
+  	'>=8.1'
+  end as "hba1c_ranges"
+FROM
+	lab_tests
