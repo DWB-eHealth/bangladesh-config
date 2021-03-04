ab919ed92e5734bdb86c990229a34630bf2fdfb6
Michaël Bontyes <michaelbontyes@gmail.com>
2021-03-04 18:02:25 -0500

    Update SCR12 to handle mixed content

--- bahmni-mart/viewSql/svr12_view.sql
+++ bahmni-mart/viewSql/svr12_view.sql
@@ -4,20 +4,21 @@ select
   hc.hcv_viral_load ,
   hc.hcv_viral_load ~ '\*.*([0-9]{3})' as "VL result numeral format" ,
   hc.hcv_viral_load ~ '([⁰¹²³⁴⁵⁶⁷⁸⁹])' as "VL result exponential format" ,
-case
-  when hc.hcv_viral_load ~ '\*.*([0-9]{3})' then
-  split_part(hc.hcv_viral_load, '*', 1)::numeric * 10 ^ right(hc.hcv_viral_load,1)::integer
-  when hc.hcv_viral_load ~ '([0-9]{3,5})' then
-  hc.hcv_viral_load::integer
-  end
-as "VL result" ,
-hc.date_of_sample_collected_for_hcv_viral_load ,
-hc2.date_of_daa_termination ,
-hc2.visit_id as "visit id date_of_daa_termination",
-case
-when hc2.date_of_daa_termination is not null then
-extract(year from age(hc.date_of_sample_collected_for_hcv_viral_load::date,hc2.date_of_daa_termination::date))*12 + extract(month from age(hc.date_of_sample_collected_for_hcv_viral_load::date,hc2.date_of_daa_termination::date))
-end as "duration_months"
+  case
+  	when not hc.hcv_viral_load ~ '([⁰¹²³⁴⁵⁶⁷⁸⁹])' and hc.hcv_viral_load ~ '\*.*([0-9]{3})' then
+  	split_part(hc.hcv_viral_load, '*', 1)::numeric * 10 ^ right(hc.hcv_viral_load,1)::integer
+  	when not hc.hcv_viral_load ~ '([⁰¹²³⁴⁵⁶⁷⁸⁹])' and hc.hcv_viral_load ~ '([0-9]{3,5})' then
+  	hc.hcv_viral_load::integer
+  	end
+  as "VL result" ,
+  hc.date_of_sample_collected_for_hcv_viral_load ,
+  hc2.date_of_daa_termination ,
+  hc2.visit_id as "visit id date_of_daa_termination",
+  case
+    when hc2.date_of_daa_termination is not null then
+	extract(year from age(hc.date_of_sample_collected_for_hcv_viral_load::date,hc2.date_of_daa_termination::date))*12 + extract(month from age(hc.date_of_sample_collected_for_hcv_viral_load::date,hc2.date_of_daa_termination::date))
+	end
+  as "duration_months"
 from hepatitis_c hc
 left outer join hepatitis_c hc2
   on hc2.patient_id = hc.patient_id
