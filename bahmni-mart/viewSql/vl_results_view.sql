f9f82b344d883a249858d85ce95c8a3714dfdd3e
Michaël Bontyes <michaelbontyes@gmail.com>
2021-03-04 18:12:36 -0500

    Updated viral load results for mixed content

--- bahmni-mart/viewSql/vl_results_view.sql
+++ bahmni-mart/viewSql/vl_results_view.sql
@@ -4,12 +4,12 @@ select
   hcv_viral_load ,
   hcv_viral_load ~ '\*.*([0-9]{3})' as "VL result numeral format" ,
   hcv_viral_load ~ '([⁰¹²³⁴⁵⁶⁷⁸⁹])' as "VL result exponential format" ,
-case
-  when hcv_viral_load ~ '\*.*([0-9]{3})' then
-  SPLIT_PART(hcv_viral_load, '*', 1)::numeric * 10 ^ right(hcv_viral_load,1)::integer
-  when hcv_viral_load ~ '([0-9]{3,5})' then
-  hcv_viral_load::integer
-  end
-as "VL result"
+  case
+  	when not hc.hcv_viral_load ~ '([⁰¹²³⁴⁵⁶⁷⁸⁹])' and hc.hcv_viral_load ~ '\*.*([0-9]{3})' then
+  	split_part(hc.hcv_viral_load, '*', 1)::numeric * 10 ^ right(hc.hcv_viral_load,1)::integer
+  	when not hc.hcv_viral_load ~ '([⁰¹²³⁴⁵⁶⁷⁸⁹])' and hc.hcv_viral_load ~ '([0-9]{3,5})' then
+  	hc.hcv_viral_load::integer
+  	end
+  as "VL result"
 from hepatitis_c hc
-where hcv_viral_load is not null 
+where hcv_viral_load is not null
