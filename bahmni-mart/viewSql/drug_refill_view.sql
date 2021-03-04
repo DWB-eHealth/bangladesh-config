ceaedacb33f2a19093fef7e36622314aaa21688e
Michaël Bontyes <michaelbontyes@gmail.com>
2021-03-04 11:00:32 -0500

    Update drug_refill_view.sql

--- bahmni-mart/viewSql/drug_refill_view.sql
+++ bahmni-mart/viewSql/drug_refill_view.sql
@@ -5,7 +5,8 @@ SELECT
 FROM
 	patient_appointment_default pad
 WHERE
-	appointment_service = 'Pharmacy - Fast Track'
+	appointment_service = 'Pharmacy - Fast Track' OR
+	appointment_service = 'Pharmacy - Fast Track(NCD)'
 GROUP BY
 	patient_id ,
 	appointment_service
