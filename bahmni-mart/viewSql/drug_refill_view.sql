75ca69faae64869b47c68d2c111aca0292b8974e
Michaël Bontyes <michaelbontyes@gmail.com>
2021-03-05 15:59:26 -0500

    Update drug_refill_view.sql

--- bahmni-mart/viewSql/drug_refill_view.sql
+++ bahmni-mart/viewSql/drug_refill_view.sql
@@ -1,15 +1,13 @@
 SELECT
 	patient_id ,
-	appointment_id ,
 	appointment_service ,
-	appointment_status ,
 	count(distinct appointment_id) as "Number of drug refill"
 FROM
 	patient_appointment_default pad
 WHERE
-	appointment_service = 'Hep C drug refill'
+	appointment_service = 'Hep C drug refill' and appointment_status = 'CheckedIn'
+	or
+	appointment_service = 'Hep C drug refill' and appointment_status = 'Completed'
 GROUP BY
 	patient_id ,
-	appointment_id ,
-	appointment_service ,
-	appointment_status
+	appointment_service
